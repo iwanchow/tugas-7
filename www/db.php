6259c4f28b5a99c073db80fdc62ffe803e5c9415
Iwan <48428707+iwanchow@users.noreply.github.com>
2019-04-29 11:18:54 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id8174847_admin","admin123","id8174847_akademik") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id9438308_iwan48	","unisbank2018","id9438308_tokobuah") or die ("could not connect database");
 ?>
